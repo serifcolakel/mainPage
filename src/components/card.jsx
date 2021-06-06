42d5af6cd3a2599b346999356580af1c4cb33e6a
Serif Colakel <serifcolakel0@gmail.com>
2021-06-06 16:56:03 +0300

    react functional component created & updated

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -6,7 +6,7 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
+import makeStyles from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 
 const cards = [
